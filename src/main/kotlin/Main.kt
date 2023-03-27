fd7458cfac63f3e4e431a1fb3d57c231ed852194
Halil Özel <halilozel1903@gmail.com>
2023-03-27 20:39:22 +0300

    Palindrome Number example added.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val range = 100..200 // define range of numbers to check for palindrome
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    for (i in range) {
+        if (isPalindrome(i)) {
+            println("$i is a palindrome number")
+        }
+    }
+}
+
+fun isPalindrome(num: Int): Boolean {
+    val str = num.toString() // convert number to string
+
+    // check if string equals its reverse
+    for (i in 0..str.length/2) {
+        if (str[i] != str[str.length-i-1]) {
+            return false
+        }
+    }
+    return true
 }
