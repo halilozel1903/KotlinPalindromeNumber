c2818dffe72f35a47437f4e8c8dd1cb70390c9e6
Halil Ozel <halilozel1903@gmail.com>
2023-12-02 16:50:21 +0300

    PalindromeNumber function short form made.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,7 @@
 fun main() {
-    val range = 100..200 // define range of numbers to check for palindrome
-
-    for (i in range) {
-        if (isPalindromeNumber(i)) {
-            println("$i is a palindrome number")
+    for (number in 100..200) {
+        if (number.toString() == number.toString().reversed()) {
+            println("$number is a palindrome number")
         }
     }
-}
-
-fun isPalindromeNumber(num: Int): Boolean {
-    val str = num.toString() // convert number to string
-
-    // check if string equals its reverse
-    for (i in 0..str.length/2) {
-        if (str[i] != str[str.length-i-1]) {
-            return false
-        }
-    }
-    return true
 }
